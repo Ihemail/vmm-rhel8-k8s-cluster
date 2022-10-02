71ea7cd5d0ca0287eaf3e6e70c5d21545fdb4f2a
ihemail <47020644+Ihemail@users.noreply.github.com>
2022-10-02 20:08:04 +0530

    Update k8s-master.sh

--- shell/k8s-master.sh
+++ shell/k8s-master.sh
@@ -4,6 +4,8 @@
  #subscription-manager clean
  #subscription-manager register --username xxxx --password xxxx --force
  #subscription-manager attach --auto
+ subscription-manager repos --disable fast-datapath-for-rhel-8-x86_64-rpms  
+ subscription-manager repos --enable fast-datapath-for-rhel-8-x86_64-rpms
  systemctl stop firewalld
  systemctl disable firewalld
  sudo sed -i '/ swap / s/^/#/' /etc/fstab
