199756516368147c6fd7b734133268620396653b
ihemail <47020644+Ihemail@users.noreply.github.com>
2022-10-04 13:51:48 +0530

    Update k8s-crio-worker.sh

--- shell/k8s-crio-worker.sh
+++ shell/k8s-crio-worker.sh
@@ -14,7 +14,7 @@
  sudo swapoff -a
  sudo setenforce 0
  sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
- sed -i '/subscription-manager/d' /root/k8s-worker.sh
+ sed -i '/subscription-manager/d' /root/k8s-crio-worker.sh
  sudo sed -i 's/reboot now#/#reboot now/g' /root/k8s-crio-worker.sh
  #reboot now#
 
