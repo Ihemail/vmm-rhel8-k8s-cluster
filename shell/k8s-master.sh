db9674cdc545ee853f47bc99050eea4c54405cc4
ihemail <47020644+Ihemail@users.noreply.github.com>
2022-10-06 19:16:19 +0530

    Update k8s-master.sh

--- shell/k8s-master.sh
+++ shell/k8s-master.sh
@@ -65,9 +65,10 @@ sleep 120
 sudo systemctl enable kubelet && systemctl restart kubelet
 echo '\ =====  Hello World :)  ===== \ '
 
+echo "kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=$eth0ip"
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$eth0ip
 # --kubernetes-version 1.24.6
-echo "kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=$eth0ip"
+
 sleep 30
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
